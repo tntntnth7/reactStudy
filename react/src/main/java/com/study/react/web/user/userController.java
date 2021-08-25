50ca7a0b886cbbea689b1bb710f99538808d4aee
HeoJun <tntntnth@naver.com>
2021-08-25 22:10:37 +0900

    push

--- react/src/main/java/com/study/react/web/user/userController.java
+++ react/src/main/java/com/study/react/web/user/userController.java
@@ -1,5 +1,6 @@
 package com.study.react.web.user;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,14 @@ public class userController {
 	
 	@GetMapping("/user")
 	public String username() {
-		return "heojun";
+		
+		String json = "{ 'A' : 'Hello Json!!',"
+				+ "'B' : '안녕하세요 제이슨',"
+				+ "'C' : 'Json Data',"
+				+ "'Boolean' : 'true',"
+				+ "'Number' : 2020"
+				+ "}";
+		
+		return json;
 	}
 }
